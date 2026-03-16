872b52ddcd3376999a1e9dca379b36845b22f328
anupam-react <react6@flyweis.technology>
2026-03-16 15:40:39 +0530

    mediasoup

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: {
+    origin: "*"
+  }
 });
 
 const rooms = {};
 
 io.on("connection", (socket) => {
 
+  console.log("User connected:", socket.id);
+
   socket.on("join-room", (roomId) => {
 
     socket.join(roomId);
 
-    if (!rooms[roomId]) {
-      rooms[roomId] = [];
-    }
+    if (!rooms[roomId]) rooms[roomId] = [];
 
     rooms[roomId].push(socket.id);
 
-    io.to(roomId).emit("users-update", rooms[roomId]);
-
     socket.to(roomId).emit("user-joined", socket.id);
 
+    io.to(roomId).emit("participants", rooms[roomId]);
   });
 
   socket.on("offer", ({ offer, to }) => {
@@ -44,17 +48,14 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
 
     for (const roomId in rooms) {
-
       rooms[roomId] = rooms[roomId].filter(id => id !== socket.id);
-
-      io.to(roomId).emit("users-update", rooms[roomId]);
-
+      io.to(roomId).emit("participants", rooms[roomId]);
     }
 
   });
 
 });
 
-server.listen(8080, () =>
-  console.log("Server running on port 8080")
-);
+server.listen(8080, () => {
+  console.log("Server running on 8080");
+});
